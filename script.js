b4d97803fb26dec56804ee38cb962fca900d62cf
Anson Yip Ann Yee <ansonyipannyee@outlook.com>
2023-09-30 15:13:04 +1000

    index files

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const keywordInput = document.getElementById('keyword-input');
+    const generateJokeBtn = document.getElementById('generate-joke-btn');
+    const jokeResult = document.getElementById('joke-result');
+
+    generateJokeBtn.addEventListener('click', () => {
+        const keyword = keywordInput.value;
+        if (keyword.trim() === '') {
+            return;
+        }
+
+        fetch(`https://icanhazdadjoke.com/search?term=${keyword}`, {
+            headers: {
+                Accept: 'application/json',
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.results && data.results.length > 0) {
+                const randomJoke = data.results[Math.floor(Math.random() * data.results.length)];
+                jokeResult.textContent = randomJoke.joke;
+            } else {
+                jokeResult.textContent = 'No jokes found with that keyword.';
+            }
+        })
+        .catch(error => console.error('Error fetching joke:', error));
+    })
+
+    const featuredJokeContent = document.getElementById('featured-joke-content');
+
+    function fetchFeaturedJoke() {
+        fetch(`https://icanhazdadjoke.com/`, {
+            headers: {
+                Accept: 'application/json',
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            featuredJokeContent.textContent = data.joke;
+        })
+        .catch(error => console.error('Error fetching joke of the day:', error));
+    }
+
+    fetchFeaturedJoke();
+
+    const likedJokesList = document.getElementById('liked-jokes-list');
+    let likedJokes = [];
+})
